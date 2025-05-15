213ee90fbd54ffd40edeea04f9fd8ee5e25ea6e9
Michal <rivki400@gmail.com>
2025-05-15 14:18:21 +0300

    change

--- src/Services/auth.service.ts
+++ src/Services/auth.service.ts
@@ -4,6 +4,7 @@ const url = 'Login'
 
 export const login = async (userName: string, password: string) => 
 {
+    //
     const response = await axios.post<string>(url + '/login', { userName, password })
     return response.data
 }
